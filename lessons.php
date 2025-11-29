fd91ef6f61a6f59a1677366bf827b826e2844e33
Tenji767 <144552671+Tenji767@users.noreply.github.com>
2025-11-29 14:45:34 -0500

    Completed on 11/29/2025

--- lessons.php
+++ lessons.php
@@ -29,3 +29,25 @@
     <a href="#progress.php"><button>See Progress</button></a>
 
 </div>
+
+<p>Pick the grade you are in</p>
+
+<div class="gradeBox">
+    <h2>1st Grade</h2>
+    <p>Counting, ordering, basic fractions, addition and subtraction, measuring, shapes, time, graphs, and patterns</p>
+</div>
+
+<div class="gradeBox">
+    <h2>2nd Grade</h2>
+    <p>Counting to 200, understanding 10s, dividing into equal parts, counting money, addition and subtraction using big numbers, measuring in American Units, time, creating 3D shapes, and more patterns.</p>
+</div>
+
+<div class="gradeBox">
+    <h2>3rd Grade</h2>
+    <p>Digits, fractions, counting money up to $5.00, addition and subtraction up to 1,000, multipication and division, area and perimeter, calculating time, polygons, and even more patterns.</p>
+</div>
+
+<div class="gradeBox">
+    <h2>4th Grade</h2>
+    <p>Nine-digit numbers, compare fractions, decimals, multi-step problems, multiplication table, multiplication of fractions, addition and subtraction of decimals, lines, quadrilaterals, line graphs, and probabilities.</p>
+</div>
